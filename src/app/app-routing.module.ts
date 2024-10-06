3ae8b1cdc54c454b8757f9f122ca09a51234a6fc
miguelcampodiaz <miguelcampodiaz@gmail.com>
2024-10-06 00:00:01 +0000

    agregando los enrrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // Ruta raíz
+  { path: 'products', component: ProductsComponent },  // Productos
+  { path: 'products/:productId', component: ProductDetailComponent },  // Detalle del producto
+  { path: 'contact', component: ContactComponent },  // Contacto
+  { path: '**', redirectTo: '', pathMatch: 'full' },  // Redirección de rutas desconocidas
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
